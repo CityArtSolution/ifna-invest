61412a9cb830c39bf1398fd93871fc20532ca738
Mohamed Elmojtaba <mohamedelmojtaba.me@gmail.com>
2023-02-14 16:45:38 +0200

    [Add] budget

---
+++ ifna_budget/__manifest__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Budget Extension',
+    'version': '15.0.0.0',
+    'category': 'Tools',
+    'author': 'City Art',
+    'depends': ['account_budget'],
+    'data': [
+        "views/budget.xml",
+            ],
+
+    'demo': [],
+    'test': [],
+}
---
+++ ifna_budget/models/crossovered_budget_lines.py
@@ -0,0 +1,29 @@
+
+# -*- coding: utf-8 -*-
+from odoo import models, fields, api, _
+
+
+class CrossoveredBudgetLines(models.Model):
+    _inherit = 'crossovered.budget.lines'
+
+    deviation_amount  = fields.Float(string='Deviation Amount', 
+        compute='_compute_deviation_amount', store=True, readonly=True)
+    deviation_ratio  = fields.Float(string='Deviation Ratio', 
+        compute='_compute_deviation_ratio', store=True, readonly=True)
+    
+    @api.depends('planned_amount', 'practical_amount')
+    def _compute_deviation_amount(self):
+        for record in self:
+            record.deviation_amount = record.planned_amount + record.practical_amount
+            
+    @api.depends('deviation_amount')
+    def _compute_deviation_ratio(self):
+        for line in self:
+            if line.deviation_amount != 0.00:
+                line.deviation_ratio = float((line.deviation_amount or 0.0) / line.planned_amount)
+            else:
+                line.deviation_ratio = 0.00
+                
+    
+
+    
