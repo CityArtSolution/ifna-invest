c7af12a6ae5c1e8a2c912d836b2bf51f02b35fca
Shaima Osama <shaima@bstt.com.sa>
2023-03-09 15:07:03 +0200

    Invoice report

--- renting/__manifest__.py
+++ renting/__manifest__.py
@@ -14,7 +14,7 @@
     'author': "Ibrahim Abdullatif",
 
     # any module necessary for this one to work correctly
-    'depends': ['base', 'sale_renting','sale_operating_unit','analytic','account_asset','l10n_gcc_invoice',],
+    'depends': ['base', 'sale_renting','sale_operating_unit','analytic','account_asset','l10n_gcc_invoice','account_operating_unit'],
 
 
     # always loaded
