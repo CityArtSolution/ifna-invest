5812a74fa81b643c21cced9b27047bd26915396d
tasneem <info@cityart.my>
2023-03-16 11:21:18 +0200

    abnaa invoices

--- invoice_report_edit/models/models.py
+++ invoice_report_edit/models/models.py
@@ -8,12 +8,6 @@ class ResBank(models.Model):
     _inherit = 'res.bank'
 
     iban = fields.Char(string='Iban')
-    holder_name = fields.Many2one("res.partner",string='Holder Name')
-    #
-    # def get_holder_name(self):
-    #     for rec in self:
-    #         banks = self.env['res.partner.bank'].search([('bank_id', '=', rec.id)], limit=1)
-    #         rec.holder_name = banks.partner_id.id
 
 
 class ExtendAccountMove(models.Model):
