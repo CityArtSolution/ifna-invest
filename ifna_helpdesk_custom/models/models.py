5b0f0a69104cbc666ac24af181a2a8959c083679
tasneem <info@cityart.my>
2023-03-21 15:22:43 +0200

    ticket product domain

--- ifna_helpdesk_custom/models/models.py
+++ ifna_helpdesk_custom/models/models.py
@@ -9,7 +9,31 @@ class HelpDeskCustom(models.Model):
         'res.users', string='Assigned to', compute='_compute_user_and_stage_ids', store=True,
         readonly=False, tracking=True)
 
+    analytic_account_id = fields.Many2one(
+        'account.analytic.account', string='الحساب التحليلي', store=True, related="product_id.analytic_account",
+        tracking=True)
+    property_id = fields.Many2one('rent.property', string='عمارة', copy=True,
+                                  related="product_id.property_id")
+
+    @api.onchange('partner_id')
+    def _get_partner_products(self):
+        for i in self:
+            products = (
+                self.env['sale.order'].search([('partner_id', '=', self.partner_id.id)]).mapped('order_line')).mapped(
+                'product_id')
+            # fees_amount = sum(self.sale_order_id.order_line.mapped(type))
+            if products:
+                if len(products) > 1:
+                    print("..................", products)
+                    return {'domain': {'product_id': [('id', 'in', products.ids)]}}
+                else:
+                    product_id = self.env["product.product"].browse(products[0])
+                    print("..................", product_id)
+                    self.product_id = product_id.id
+
+
 class HelpDeskCustomType(models.Model):
     _inherit = 'helpdesk.ticket.type'
 
-    helpdesk_teams_ids = fields.Many2many(comodel_name="helpdesk.team", relation="rel_team_type", string="Helpdesk Team", )
+    helpdesk_teams_ids = fields.Many2many(comodel_name="helpdesk.team", relation="rel_team_type",
+                                          string="Helpdesk Team", )
