4e27bf6755cc52fc02cb8890d136bedc59d4f87d
samah kandel <samahqandeel22@gmail.com>
2023-03-21 16:18:47 +0200

    [ADD].

--- account_dynamic_edited/models/models.py
+++ account_dynamic_edited/models/models.py
@@ -40,6 +40,7 @@ class AccountGroupLevel(models.Model):
         ('unique_name', 'unique (name)', "You can't create an Level with an existing Number, Please change it.")
     ]
 
+
 class Journal(models.Model):
     _inherit = 'account.move'
 
@@ -50,14 +51,20 @@ class Journal(models.Model):
             if rec.move_type == 'entry':
                 for line in rec.line_ids:
                     lines.append({
-                         line.account_id:line.debit + line.credit,
+                        line.account_id.name:line.debit + line.credit,
+                        # line.account_id.name:[line.debit + line.credit,line.analytic_account_id.name]
+                        line.analytic_account_id.name:''
                     })
-                print(lines)
                 for order in lines:
                     for item in order:
-                        if item in lines_dict:
+                        if item in lines_dict :
                             lines_dict[item] += order[item]
+                            if item[1:] in lines_dict:
+                                lines_dict[item] += order[item]
+                                lines_dict[item].append(item.analytic_account_id)
                         else:
                             lines_dict[item] = order[item]
+                            # lines_dict[item.analytic_account_id] = order[item.analytic_account_id]
+
                 print(lines_dict)
-            return lines_dict
+                return lines_dict
--- account_new_journal/models/models.py
+++ account_new_journal/models/models.py
@@ -3,6 +3,7 @@
 import time
 from odoo import models, fields, _, api
 from odoo.exceptions import ValidationError
+from itertools import groupby
 
 
 class Journal(models.Model):
@@ -10,19 +11,41 @@ class Journal(models.Model):
 
     def calculate_total_account(self):
         lines_dict = {}
+        dataaa = []
         lines = []
         for rec in self:
             if rec.move_type == 'entry':
                 for line in rec.line_ids:
                     lines.append({
-                         line.account_id:line.debit + line.credit,
+                        line.account_id.name:line.debit + line.credit,
+                        # line.account_id.name:[line.debit + line.credit,line.analytic_account_id.name]
+                        line.analytic_account_id.name:''
                     })
-                print(lines)
                 for order in lines:
                     for item in order:
-                        if item in lines_dict:
+                        if item in lines_dict :
                             lines_dict[item] += order[item]
+                            if item[1:] in lines_dict:
+                                lines_dict[item] += order[item]
+                                lines_dict[item].append(item.analytic_account_id)
                         else:
                             lines_dict[item] = order[item]
+                            # lines_dict[item.analytic_account_id] = order[item.analytic_account_id]
+
                 print(lines_dict)
-            return lines_dict
+                return lines_dict
+
+
+
+
+
+
+
+
+# {'Current Assets': 100.0, 'تـأمين': account.analytic.account(21,),
+#  'Account Receivable (PoS)': 100.0, 'رسوم ادارية': account.analytic.account(19,),
+#  'Bank': 200.0, 'رسوم ادارية خاضعة': account.analytic.account(23,)}
+
+# {'Current Assets': 300.0, 'تـأمين': account.analytic.account(21, 21),
+# 'Account Receivable (PoS)': 100.0, 'رسوم ادارية': account.analytic.account(19,),
+# 'Bank': 200.0, 'رسوم ادارية خاضعة': account.analytic.account(23,)}
