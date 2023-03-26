eada616c202ca4aee9a64aedbda52726b9550d47
samah kandel <samahqandeel22@gmail.com>
2023-03-26 12:50:57 +0200

    [ADD].

--- account_new_journal/models/models.py
+++ account_new_journal/models/models.py
@@ -37,41 +37,8 @@ class Journal(models.Model):
                                                                      fields=['account_id','analytic_account_id',
                                                                              'credit', 'debit'],
                                                                      groupby=['account_id','analytic_account_id'],lazy=False,)
-
-                new = rec.env['account.move.line'].read_group(domain=[('move_id', '=', rec.id)],
-                                                              fields=['analytic_account_id', 'debit', 'credit'],
-                                                              groupby=['analytic_account_id'])
-
-            print('lines', lines_dict)
-            print('new', new)
-            for line in lines_dict:
-                print('account', line.get('account_id'))
-                print('account analytic', line.get('analytic_account_id'))
+            # print(lines_dict)
+            # for line in lines_dict:
+            #     print('debit', line.get('debit'))
+            #     print('credit', line.get('credit'))
             return lines_dict
-
-    # def calculate_total_account(self):
-    #     lines_dict = {}
-    #     dataaa = []
-    #     lines = []
-    #     for rec in self:
-    #         if rec.move_type == 'entry':
-    #             for line in rec.line_ids:
-    #                 lines.append({
-    #                     line.account_id.name:line.debit + line.credit,
-    #                     # line.account_id.name:[line.debit + line.credit,line.analytic_account_id.name]
-    #                     line.analytic_account_id.name:''
-    #                 })
-    #             for order in lines:
-    #                 for item in order:
-    #                     if item in lines_dict :
-    #                         # lines_dict[item] += order[item]
-    #                         # if item[1:] in lines_dict:
-    #                             lines_dict[item] += order[item]
-    #                             # lines_dict[item].append(item.analytic_account_id)
-    #                     else:
-    #                         lines_dict[item] = order[item]
-    #                         # lines_dict[item.analytic_account_id] = order[item.analytic_account_id]
-    #
-    #             print(lines_dict)
-    #             return lines_dict
-
