3f86dbe517956fc55abaf707943a4d3c4dae04da
hisham <you@example.com>
2023-05-21 17:43:59 +0300

    Request payments - notes on screen and print out

---
+++ renting_workflow_customization/__manifest__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Renting Workflow Customization",
+    'summary': """Sale Renting Workflow""",
+    'category': 'Sales Management',
+    'version': '0.1',
+    'depends': ['sale_renting','renting'],
+    'data': [
+        # 'security/security.xml',
+        'views/sales_views.xml',
+    ]
+}
