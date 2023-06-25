1ec15be1759922757cdfa2b13247b33902334247
Mohamed Elmojtaba <mohamedelmojtaba.me@gmail.com>
2023-06-25 03:35:41 +0200

    [mod] line sequence

--- plus_year/models/models.py
+++ plus_year/models/models.py
@@ -1,14 +1,16 @@
 # -*- coding: utf-8 -*-
 
-from odoo import models, fields, api ,_
+from odoo import models, fields, api
 from dateutil.relativedelta import relativedelta
-
+from odoo.exceptions import UserError, ValidationError
 
 class RentSaleOrder(models.Model):
     _inherit = 'sale.order'
 
-    plus_year = fields.Boolean(string="اكثر من سنة")
-    years_number = fields.Integer(string="عدد السنين")
+    plus_year = fields.Boolean(string="اكثر من سنة", store=True)
+    years_number = fields.Integer(string="عدد السنين", store=True)
+    years = fields.Char(string='Years', store=True)
+
 
     @api.onchange('years_number', 'fromdate')
     def _compute_to_date(self):
@@ -19,69 +21,65 @@ class RentSaleOrder(models.Model):
 
 class RentSaleOrderLine(models.Model):
     _inherit = 'sale.order.line'
+    _order = 'line_sequence'
 
-    years_number = fields.Integer(related="order_id.years_number", store=True, readonly=False)
+    year_number = fields.Integer(related="order_id.years_number", store=True, readonly=False)
     fromdate = fields.Datetime(string='From Date', copy=True)
     todate = fields.Datetime(string='To Date', copy=True)
+    test = fields.Integer(related='order_id.years_number',readonly=True,store=True)
+    line_sequence = fields.Integer(string='Line Sequence', copy=False, default=100)
 
+    @api.model
+    def create(self, values):
+        order = values['order_id']
+        if 'line_sequence' in values and 'property_number' in values:
+            values['line_sequence'] = self._get_next_line_sequence(order)
+        return super().create(values)
 
-    @api.onchange('years_number')
-    def test(self):
-        print('fromdate',self.fromdate)
-        nums = []
-        print("///////////////////",self.fromdate)
-        print("///////////////////",self.years_number)
-        for x in range(1, (self.years_number)+1):
-            nums.append(x)
-        selection = [(num, num) for num in nums]
-        print("////////////////////////////////////",selection)
-        return selection
-
-    # # @api.depends("years_number")
-    # def get_years_selection(self):
-    #
-    #     nums = []
-    #     print("///////////////////",self.fromdate)
-    #     for x in range(1, self.years_number):
-    #         nums.append(x)
-    #     selection = [(num, num) for num in nums]
-    #     return selection
-
-    # year_number = fields.Integer(string="رقم السنة")
-    year_number = fields.Selection(string="رقم السنة", selection=test)
-
+    @api.model
+    def _get_next_line_sequence(self, order):
+        last_line = self.env['sale.order.line'].search([('order_id', '=', order),('property_number', '!=', False)], order='line_sequence desc', limit=1)
+        if last_line:
+            return last_line.line_sequence + 100
+        return 100
+    
     @api.onchange('year_number')
-    def _compute_dates(self):
-        for rec in self:
+    def check_year_number(self):
+        if not (self.year_number >= 0 and self.year_number <= self.order_id.years_number):
+            raise UserError(f"رقم السنه يجب ان يكون اقل من او يساوي {self.order_id.years_number}")
+    
+    # @api.onchange('year_number')
+    # def _compute_dates(self):
+    #     for rec in self:
 
-            if rec.year_number and rec.order_id.fromdate:
-                if rec.year_number == 1:
-                    fromdate = rec.fromdate = rec.order_id.fromdate
-                    rec.todate = fromdate + relativedelta(years=1)
+    #         if rec.year_number and rec.order_id.fromdate:
+    #             if rec.year_number == 1:
+    #                 fromdate = rec.fromdate = rec.order_id.fromdate
+    #                 rec.todate = fromdate + relativedelta(years=1)
 
-                if rec.year_number == 2:
-                    date = self.env['sale.order.line'].search([('year_number', '=', 1)], limit=1).mapped('todate')
-                    if date:
-                        fromdate = rec.fromdate = date[0]
-                        rec.todate = fromdate + relativedelta(years=1)
+    #             if rec.year_number == 2:
+    #                 date = self.env['sale.order.line'].search([('year_number', '=', 1)], limit=1).mapped('todate')
+    #                 if date:
+    #                     fromdate = rec.fromdate = date[0]
+    #                     rec.todate = fromdate + relativedelta(years=1)
 
-                if rec.year_number == 3:
-                    date = self.env['sale.order.line'].search([('year_number', '=', 2)], limit=1).mapped('todate')
-                    if date:
-                        fromdate = rec.fromdate = date[0]
-                        rec.todate = fromdate + relativedelta(years=1)
+    #             if rec.year_number == 3:
+    #                 date = self.env['sale.order.line'].search([('year_number', '=', 2)], limit=1).mapped('todate')
+    #                 if date:
+    #                     fromdate = rec.fromdate = date[0]
+    #                     rec.todate = fromdate + relativedelta(years=1)
 
-                if rec.year_number == 4:
-                    date = self.env['sale.order.line'].search([('year_number', '=', 3)], limit=1).mapped('todate')
-                    if date:
-                        fromdate = rec.fromdate = date[0]
-                        rec.todate = fromdate + relativedelta(years=1)
+    #             if rec.year_number == 4:
+    #                 date = self.env['sale.order.line'].search([('year_number', '=', 3)], limit=1).mapped('todate')
+    #                 if date:
+    #                     fromdate = rec.fromdate = date[0]
+    #                     rec.todate = fromdate + relativedelta(years=1)
 
-                if rec.year_number == 5:
-                    date = self.env['sale.order.line'].search([('year_number', '=', 4)], limit=1).mapped('todate')
-                    if date:
-                        fromdate = rec.fromdate = date[0]
-                        rec.todate = fromdate + relativedelta(years=1)
+    #             if rec.year_number == 5:
+    #                 date = self.env['sale.order.line'].search([('year_number', '=', 4)], limit=1).mapped('todate')
+    #                 if date:
+    #                     fromdate = rec.fromdate = date[0]
+    #                     rec.todate = fromdate + relativedelta(years=1)
 
     # @api.constrains('year_number')
     # def _check_year_number(self):
