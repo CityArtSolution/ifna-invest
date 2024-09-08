53e0796f270019b2df0b1cbac8d221c020fbdb07
Manara-Mohamed <mnarmhmd1234@gmail.com>
2024-09-08 14:32:43 +0300

    last updates new

--- legal_affairs_management/static/src/js/custom.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-    // $('#legal_case_table').DataTable({
-    //     "paging": true,         // Enable pagination
-    //     "searching": true,      // Enable search box
-    //     "ordering": true,       // Enable sorting
-    //     "info": true            // Show information (e.g., "Showing 1 to 10 of 50 entries")
-    // });
-});
