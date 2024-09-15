c5a576e1eddc25915b0fb7c89fac3d898a457a07
Manara-Mohamed <mnarmhmd1234@gmail.com>
2024-09-15 12:15:53 +0300

    renting

--- ifna_payment_custom/__manifest__.py
+++ ifna_payment_custom/__manifest__.py
@@ -1,16 +1,22 @@
-# -*- coding: utf-8 -*-
-
+# -*- coding: utf-8 -*-\
 {
-    'name': 'Custom Payment Order',
+    'name': "Custom Payment Order",
+    'summary': """Custom Payment Order""",
+    'description': """Custom Payment Order""",
+    'author': "City Art",
+    'website': "",
+    'category': 'Uncategorized',
     'version': '15.0.0.0',
-    'category': 'Tools',
-    'author': 'City Art',
     'depends': ['web', 'base', 'account','account_payment_order'],
+    'qweb': [],
     'data': [
-        "views/views.xml",
-        "views/report_payment.xml",
-             ],
-
-    'demo': [],
-    'test': [],
+        "views/account_payment_order_view.xml",
+        "report/account_payment_order_report.xml",
+    ],
+    'images': ['static/description/banner.png'],
+    'license': 'OPL-1',
+    'sequence': 5,
+    'auto_install': False,
+    'installable': True,
+    'application': True,
 }
