840d2b3bc8d483ed4b9427e7fcdc188257639258
Manara-Mohamed <mnarmhmd1234@gmail.com>
2024-09-16 22:41:36 +0300

    state

--- legal_affairs_management/models/legal_execution_request.py
+++ legal_affairs_management/models/legal_execution_request.py
@@ -39,7 +39,7 @@ class LegalExecutionRequest(models.Model):
         ('not_paid', 'Not Paid'),
         ('partial', 'Partial'),
         ('other', 'Other'),
-    ], string='State', compute="_compute_state", tracking=True)
+    ], string='State', compute="_compute_state", search=True, tracking=True)
 
     account_journal_count = fields.Integer(compute="_compute_account_journal_count")
     journal_id = fields.Many2one('account.journal', 'Account Journal', domain=[('type', 'in', ('bank', 'cash'))], tracking=True)
